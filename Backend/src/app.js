5a35d438845b2babedcedebc9f033eed05f79daa
Sanket Vishwakarma <your-email@example.com>
2026-02-06 17:46:48 +0530

    .

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -77,7 +77,7 @@ app.patch("/api/notes/:id", async (req, res) => {
 
 // Serving static files
 app.use("*name", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "/public/index.html"));
 });
 
 module.exports = app;
